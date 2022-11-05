99073bbb8cf773001cc21ba41e06733773796a46
Your Name <your.email@your.com>
2022-11-05 18:04:41 +0000

    Add --privileged switch to docker run

--- run_bash.sh
+++ run_bash.sh
@@ -2,7 +2,11 @@
 
 SCRIPT_PATH=$(dirname $(realpath -s $0))
 
+# Note that we need `--privileged` below to check for 
+# the silencer dongle at `/dev/usb/` from inside 
+# the container.
 docker run --rm -it \
+  --privileged \
   -v $SCRIPT_PATH/data:/data \
   -v $SCRIPT_PATH/src:/app \
   -v $SCRIPT_PATH/tesla:/tesla \
